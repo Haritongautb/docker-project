9c5ac9bec37c12804913f0e4ccb70f464694f47d
Haritongautb <khariton.aaa.97@gmail.com>
2024-04-18 19:53:45 +0200

    changes

--- models/User.js
+++ models/User.js
@@ -1,38 +1,40 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     fullName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     passwordHash: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     avatarURL: String,
     role: {
-        enum: ['admin', 'owner', 'user'],
-        type: String,
-        default: 'user'
+      enum: ["admin", "owner", "user"],
+      type: String,
+      default: "user",
     },
     subscribed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     receipts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Receipt"
-        }
-    ]
-}, {
-    timestamps: true
-});
-
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Receipt",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", UserSchema);
